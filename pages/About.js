864ca4bc280aa4233477c3bfc828c5cbb2593834
Cihsan <mutamuty@gmail.com>
2022-07-20 01:39:23 +0600

    nextjs tailwind daisyui install

---
+++ pages/About.js
@@ -0,0 +1,11 @@
+
+const About = () => {
+    return (
+        <div>
+            <h1 className="text-5xl font-bold underline">About</h1>
+            <button className="btn btn-secondary">Button</button>
+        </div>
+    );
+};
+
+export default About;
