cd5993cd1befdc4f7c611ade4618e17e1a167b8c
fahimshahriar786 <fahimshahariar786@gmail.com>
2022-08-05 00:19:25 +0600

    Financeapp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import EmployeHome from './EmployeHome/EmployeHome';
+import MotherFinance from "./MotherFinanceApp/MotherFinance"
 // import Dashboard from './Page/Dashboard';
 // import EmployMToolDashboard from "./EmployMenagemant Tool/EmployMToolDashboard"
 function App() {
   return (
     <div>
       <EmployeHome></EmployeHome>
+      <MotherFinance></MotherFinance>
       
     </div>
   );
---
+++ src/MotherFinanceApp/MotherFinance.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import FinanceApp from './FinanceApp/FinanceApp';
+import { Provider } from './FinanceApp/context/context';
+import { SpeechProvider } from '@speechly/react-client';
+
+const MotherFinance = () => {
+    return (
+        <div>
+             <SpeechProvider appId="7c4aee08-1073-4a32-b862-ebe1850e0732" language="en-US">
+    <Provider>
+    <FinanceApp></FinanceApp>
+    </Provider>
+  </SpeechProvider>,
+        </div>
+    );
+};
+
+export default MotherFinance;
